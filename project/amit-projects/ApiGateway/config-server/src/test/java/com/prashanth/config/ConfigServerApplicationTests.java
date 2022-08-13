36d09ff1514d9c95052f20d6431556a9e2ef01e6
Prashanth0419 <103980443+Prashanth0419@users.noreply.github.com>
2022-08-13 10:01:45 +0530

    Add files via upload

---
+++ project/amit-projects/ApiGateway/config-server/src/test/java/com/prashanth/config/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prashanth.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ project/amit-projects/ApiGateway/zipkin-server/src/test/java/com/prashanth/zipkin/ZipkinServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prashanth.zipkin;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipkinServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
